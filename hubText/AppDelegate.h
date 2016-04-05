11b2a521a4ca36498eec369e653815958087a275
DC012 <dc012@run.lan>
2016-04-05 08:54:32 +0800

    Initial Commit

---
+++ hubText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hubText
+//
+//  Created by DC012 on 16/4/5.
+//  Copyright © 2016年 DC012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hubText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hubText
+//
+//  Created by DC012 on 16/4/5.
+//  Copyright © 2016年 DC012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
